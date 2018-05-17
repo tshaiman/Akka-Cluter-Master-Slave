912ac07706cd314604fe299e0850daae5008c39c
Tomer Shaiman <tomer.shaiman@doubleverify.com>
2018-05-17 18:09:45 +0300

    avro

---
+++ edge/src/test/scala/Pizza.scala
@@ -0,0 +1,4 @@
+package com.dv.test.data
+
+case class Pizza(name:String ,toping:String, calories:Int)
+//case class Pizza(name:String ,toping:String, calories:Int,rate:Option[String],geo:Option[String])
